83e30c26b54f3a4719042e454744c9c355a0cd2b
gabboraron <gabboraron@gmail.com>
2017-11-16 12:18:44 +0100

    Add files via upload

--- rowsMultiplication.java
+++ rowsMultiplication.java
@@ -8,7 +8,8 @@ public class rowsMultiplication{
 			System.out.println("Too much arguments!");
 		} else {
 			try{
-				rowsMultiplicator(args[0]);
+				//rowsMultiplicator(args[0]);
+				rowsMultiplicatorWithBufferedReader(args[0]);
 			} catch(IOException e) {
 				System.err.println("Bad filename, or something else :)");
 				e.printStackTrace();
@@ -43,4 +44,32 @@ public class rowsMultiplication{
 		}
 	}
 	
+	
+	public static void rowsMultiplicatorWithBufferedReader(String input)
+	throws IOException{
+		File inFile = new File(input);
+		
+		try ( BufferedReader br = new BufferedReader(new FileReader(inFile));){
+			String line;
+			for (line = br.readLine(); line != null; line = br.readLine() ) {			
+				String line1;
+				line1 = line;
+				line = br.readLine();
+				if(line != null){
+					System.out.println(line1);
+					System.out.println(line);
+					String[] res1 = line1.split(",");
+					String[] res2 = line.split(",");
+					
+					int[] tmp = new int[res1.length];
+					for(int i=0; i<res1.length; ++i){
+						tmp[i] = Integer.parseInt(res1[i]) * Integer.parseInt(res2[i]);
+					}
+					System.out.println("*****");
+					System.out.println(Arrays.toString(tmp));
+					System.out.println(" ");
+				}
+			}
+		}
+	}
 }
